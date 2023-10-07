0649d9da3622b99a9d6b4a738c64638721023f82
Elenaudoel <137452488+Elenaudoel@users.noreply.github.com>
2023-10-07 10:03:08 -0300

    calculos-Aula_'8'

---
+++ Aula_8/script.js
@@ -0,0 +1,26 @@
+//document.getElementById('titulo').innerHTML = 'bom dia'
+//let resposta = window.prompt('Digite um numero')
+//document.write(5 + 5)
+//console.log('teste 123')
+//console.log(resposta)
+//document.write(resposta)
+
+//let resposta = window.prompt('Digite um numero')
+//let num = resposta %  2;
+
+//if(num == 0)
+ //document.write('e par')
+//else
+ //document.write('e impar')
+
+//let num = parseInt(window.prompt('Digite um numero'))
+//let num1 = parseInt(window.prompt('Digite um numero'))
+
+//let resposta = num + num1 
+//document.write(resposta)
+
+let n1 = parseInt(window.prompt('Digite um numero'))
+for(let n2 = 1 ; n2 < 11;n2++){
+    
+    document.write(n1 * n2)
+}
