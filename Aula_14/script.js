55be9c9e4a9f3716546c5252a8b5f61b82b19aa8
Elenaudoel <137452488+Elenaudoel@users.noreply.github.com>
2023-12-09 09:31:38 -0300

    fim da pokedex Aula15

--- Aula_14/script.js
+++ Aula_14/script.js
@@ -12,14 +12,46 @@ const buscar_pokemon = async (pokemon) =>{
     resposta  = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
     if (resposta.status === 200){
         const info = resposta.json()
+        console.log(info)
         return info
     }
 }
 const mostrar_pokemon = async (pokemon) =>{
+    NAME_POKE.innerHTML = 'Pesquisando...'
+    NUMBER_POKE.innerHTML = ''
     const json_pokemon = await buscar_pokemon(pokemon)
     if(json_pokemon){
-        IMG_POKE.scr = json_pokemon['spites']['versions']['geration-v']['black-white']['animated']['front_default']
+        IMG_POKE.src = json_pokemon['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+        IMG_POKE.style.display = 'block'
+        NAME_POKE.innerHTML = json_pokemon.name
+        NUMBER_POKE.innerHTML = json_pokemon.id
+        id_pokemon = json_pokemon.id
+        INPUT.value = ''
+    }
+    else{
+        IMG_POKE.style.display = 'nome'
+        NAME_POKE.innerHTML = 'Not Founded'
+        NUMBER_POKE.innerHTML = ''
     }
 }
 
-buscar_pokemon('pikachu')
+FORM.addEventListener('submit', (evento) => {
+    evento.preventDefault()
+    let pokemon = INPUT.value.toLowerCase()
+    mostrar_pokemon(pokemon)
+})
+
+PRESV_BTN.addEventListener('click', () => {
+    if(id_pokemon > 1){
+        id_pokemon -= 1
+        mostrar_pokemon(id_pokemon)
+    }
+})
+
+NEXT_BTN.addEventListener('click', () => {
+    id_pokemon += 1 
+    mostrar_pokemon(id_pokemon)
+
+})
+
+mostrar_pokemon(id_pokemon)
